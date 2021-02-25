7538742615ce35daa873f314b12087882a4e7e61
tj513 <tj5134@naver.com>
2021-02-25 10:23:41 +0900

    Test3

--- src/LottoFrame.java
+++ src/LottoFrame.java
@@ -5,7 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-// 3조
+// 3조 lotto
 public class LottoFrame extends JFrame {
 
 	private JPanel contentPane;
