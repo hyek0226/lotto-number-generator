5d6f876e93ee4f1c6ab676681533c42f5d4762d6
user <user@DESKTOP-OESO9LM>
2021-02-25 11:09:00 +0900

    주석추가

--- src/LottoFrame.java
+++ src/LottoFrame.java
@@ -5,7 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-// 3조 lotto
+// 3議� lotto
+// 김은채 
 public class LottoFrame extends JFrame {
 
 	private JPanel contentPane;
